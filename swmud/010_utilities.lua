7c3b89437c3eb20b92bcfd7cbdd6c8e2f735cf8b
Miko <michael.kubiske@blackmesalab.com>
2024-01-03 14:03:47 -0500

    bug: uptime catch fix

--- swmud/010_utilities.lua
+++ swmud/010_utilities.lua
@@ -102,7 +102,7 @@ function CONVERT_MUD_DATE(string_in)
   -- string_in="Sat Nov  4 00:01:15 2023 EST"
   --           (Thu Dec 28 12:30:00 2023 EST)
   local date={}
-  date['month'],date['day'],date['hour'],date['min'],date['sec'],date['year']=string_in:match("%a+ (%a+) (%d+) (%d+):(%d+):(%d+) (%d+)")
+  date['month'],date['day'],date['hour'],date['min'],date['sec'],date['year']=string_in:match("%a+ (%a+)[ ]*(%d) (%d+):(%d+):(%d+) (%d+)")
   local MON={Jan=1,Feb=2,Mar=3,Apr=4,May=5,Jun=6,Jul=7,Aug=8,Sep=9,Oct=10,Nov=11,Dec=12}
   date['month']=MON[date['month']]
   date["isdst"] = false 
