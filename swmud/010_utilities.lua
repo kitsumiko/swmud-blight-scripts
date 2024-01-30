74a1f086aa783db87006a674fcad706a75ffba30
Miko <michael.kubiske@blackmesalab.com>
2024-01-30 10:50:33 -0500

    bug: uptime spacing bug

--- swmud/010_utilities.lua
+++ swmud/010_utilities.lua
@@ -101,8 +101,13 @@ end
 function CONVERT_MUD_DATE(string_in)
   -- string_in="Sat Nov  4 00:01:15 2023 EST"
   --           (Thu Dec 28 12:30:00 2023 EST)
+           --  (Thu Feb 22 22:56:13 2024 EST)
+
+  -- SWmud has been up for:  5d 15h 33m  3s.  Memory Usage: 516.7 MB
+  -- Next scheduled reboot:  23d 12h 26m 57s. (Thu Feb 22 22:56:13 2024 EST)
+  -- blight.output(string_in)
   local date={}
-  date['month'],date['day'],date['hour'],date['min'],date['sec'],date['year']=string_in:match("%a+ (%a+)[ ]*(%d) (%d+):(%d+):(%d+) (%d+)")
+  date['month'],date['day'],date['hour'],date['min'],date['sec'],date['year']=string_in:match("%a+ (%a+)%s+(%d+) (%d+):(%d+):(%d+) (%d+)")
   local MON={Jan=1,Feb=2,Mar=3,Apr=4,May=5,Jun=6,Jul=7,Aug=8,Sep=9,Oct=10,Nov=11,Dec=12}
   date['month']=MON[date['month']]
   date["isdst"] = false 
